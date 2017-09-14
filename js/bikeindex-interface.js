a5ec8371978a67eeabd781808c575b8be3e578ec
Haneen AbuKhater and Jessica Sheridan <jessica@>
2017-09-14 16:24:03 -0700

    chart.js for api data

--- js/bikeindex-interface.js
+++ js/bikeindex-interface.js
@@ -1,10 +1,11 @@
 import { BikeIndex } from './../js/bikeindex.js';
+import { Chart } from './../node_modules/chart.js';
 
 $(document).ready(function() {
 
   let displayData = function(bikes){
    bikes.forEach(function(bike){
-     console.log(bike);
+    //  console.log(bike);
      let dateStolen = moment.unix(bike.date_stolen).format('MM-DD-YYYY');
     $('ul#bike-list').append(`<li> <span class="bikes"> ${bike.title} ${dateStolen} ${bike.serial} </span> </li>`);
      // for (let key in bike) {
@@ -15,6 +16,50 @@ $(document).ready(function() {
    });
  };
 
+ //Chart.js
+   let displayChart = function (chartData) {
+     console.log("inside DisplayChart"+chartData);
+     var ctx = document.getElementById("myChart").getContext('2d');
+     var myChart = new Chart(ctx, {
+     type: 'bar',
+     data: {
+         labels: ["January", "February", "March", "April", "May", "June","July","August","September","October","November","December"],
+         datasets: [{
+             label: 'Bikes Stolen By Month',
+             data: [chartData[0], chartData[1], chartData[2], chartData[3], chartData[4], chartData[5], chartData[6], chartData[7], chartData[8], chartData[9], chartData[10], chartData[11], chartData[12]],
+             backgroundColor: [
+                 'rgba(255, 99, 132, 0.2)',
+                 'rgba(54, 162, 235, 0.2)',
+                 'rgba(255, 206, 86, 0.2)',
+                 'rgba(75, 192, 192, 0.2)',
+                 'rgba(153, 102, 255, 0.2)',
+                 'rgba(255, 159, 64, 0.2)'
+             ],
+             borderColor: [
+                 'rgba(255,99,132,1)',
+                 'rgba(54, 162, 235, 1)',
+                 'rgba(255, 206, 86, 1)',
+                 'rgba(75, 192, 192, 1)',
+                 'rgba(153, 102, 255, 1)',
+                 'rgba(255, 159, 64, 1)'
+             ],
+             borderWidth: 1
+         }]
+     },
+     options: {
+         scales: {
+             yAxes: [{
+                 ticks: {
+                     beginAtZero:true
+                 }
+             }]
+         }
+     }
+   });
+
+ };
+
+
   $('#bike-search').submit(function(event) {
     event.preventDefault();
     $("#bike-list li").remove();
@@ -22,13 +67,15 @@ $(document).ready(function() {
     let inputZipcode = $('#zipcode').val();
     let bikeIndex = new BikeIndex(inputZipcode);
 
-    bikeIndex.getBikes(displayData);
+    bikeIndex.getBikes(displayData,displayChart);
+    bikeIndex.getGraphData();
+
 
 // We do not have access to the bikes array so cannot render the
 //  individual bike details. if I put this click event handler inside
-// the displayData function....it loops through all the li's on one click 
+// the displayData function....it loops through all the li's on one click
     $("#bike-list").last().on('click', '.bikes',function(){
-      console.log(`Bike info ${bike.title}`);
+      // console.log(`Bike info ${bike.title}`);
       alert("you are good");
       $(".show-bike").show();
       $(".show-bike h4").text("test");
--- js/bikeindex.js
+++ js/bikeindex.js
@@ -18,18 +18,107 @@ export class BikeIndex{
     return bikeArray;
   }
 
-  getBikes(displayData){
+  getChartData(bikes){
+    let numBikes = [];
+    for (let i = 0 ; i < 12; i++) {
+      numBikes[i] = 0;
+    }
+    bikes.forEach(function(bike) {
+      let dateStolen = moment.unix(bike.date_stolen).format('MM');
+      let date = parseInt(dateStolen) - 1;
+      numBikes[date] += 1;
+    });
+    for (let i = 0 ; i < 12; i++) {
+      //console.log("numBikes"+i+"  "+numBikes[i]);
+    }
+    return numBikes;
+  }
+
+  getBikes(displayData,displayChart){
     let filteredBikes;
+    let chartData;
     let results;
-    let url = `https://bikeindex.org:443/api/v3/search?location=${this.zipcode}&distance=10&stolenness=proximity`;
+
+    let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=${this.zipcode}&distance=10&stolenness=proximity`;
     $.get(url)
       .then( (results) => {
       filteredBikes = this.filterBikeData(results);
       displayData(filteredBikes);
+      chartData = this.getChartData(filteredBikes);
+      displayChart(chartData);
     })
       .fail( () => {
-        console.log("something went wrong");
-        filteredBikes = [];
+        console.log("getBikes: something went wrong");
       });
     }
-}
+  }
+// nested .get not working, can't access displayData
+    // getCount(displayData,displayChart){
+    //   let counts;
+    //   let totalProximityPage;
+    //   let loopOut;
+    //   let filteredBikes;
+    //   let chartData;
+    //   let totalUrl = `https://bikeindex.org:443/api/v3/search/count?location=${this.zipcode}&distance=10&stolenness=proximity`;
+    //
+    //   $.get(totalUrl)
+    //     .then( (counts, displayData) => {
+    //       totalProximityPage = parseInt(counts.proximity)/100;
+    //       console.log("TOTAL"+totalProximityPage.toFixed(0));
+    //       for ( let i = 1; i < 3; i++){
+    //         $.get(`https://bikeindex.org:443/api/v3/search?page=${i}&per_page=100&location=${this.zipcode}&distance=10&stolenness=proximity`)
+    //           .then ( (loopOut, displayData) => {
+    //           filteredBikes = this.filterBikeData(loopOut);
+    //           this.displayData(filteredBikes);
+    //           chartData = this.getChartData(filteredBikes);
+    //           displayChart(chartData);
+    //           console.log("APILoop"+loopOut);
+    //         })
+    //         .fail( () => {
+    //           console.log("inLoop: something went wrong");
+    //         });
+    //       }
+    //     })
+    //     .fail( () => {
+    //       console.log("getCount: something went wrong");
+    //     });
+    //   }
+    //
+//Try Javascript with Promises
+  //getGraphData() {
+  //----------get Total items
+      //   let promise0 = new Promise((resolve, reject)=> {
+      //   let request = new XMLHttpRequest();
+      //   let totalUrl = `https://bikeindex.org:443/api/v3/search/count?location=${this.zipcode}&distance=10&stolenness=proximity`;
+      //     request.onload = () => {
+      //       if (this.status === 200) {
+      //         resolve(request.response); //promise0 equals Total
+      //       } else {
+      //         reject(Error(request.statusText));
+      //       }
+      //     }
+      //     request.open("GET", totalUrl, true);
+      //     request.send();
+      //     console.log("PROMISE0 total response"+request.response.proximity);
+      //   });
+      // }
+        // totalProximityPage = parseInt(request.response.proximity)/100;
+        // console.log("TOTAL"+totalProximityPage.toFixed(0));
+        // for ( let i = 1; i < 3; i++){
+        //   $.get(`https://bikeindex.org:443/api/v3/search?page=${i}&per_page=100&location=${this.zipcode}&distance=10&stolenness=proximity`)
+        // }
+    //------------iterate thru all pages------------
+        // let promise = new Promise(function(resolve, reject) {
+        // let request = new XMLHttpRequest();
+        // let url =  ;
+        // request.onload = function() {
+        //   if (this.status === 200) {
+        //     resolve(request.response);
+        //   } else {
+        //     reject(Error(request.statusText));
+        //   }
+        // }
+        // request.open("GET", url, true);
+        // request.send();
+        // });
+     //}
